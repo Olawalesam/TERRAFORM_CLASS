b4a3ed8b80595e10dbc5d70caafe3702a8e82baf
Olawalesam <olawalesam@yahoo.com>
2024-05-12 23:27:21 +0100

    My first real commit

--- main.tf
+++ main.tf
@@ -18,12 +18,12 @@ resource "azurerm_resource_group" "terabitsrg" {
 resource "azurerm_virtual_network" "terabitsvnet" {
    name                = "terabits-vnet"
    address_space       = ["10.0.0.0/16"]
-   location            = azurerm_resource_group.myrg.location
-   resource_group_name = azurerm_resource_group.myrg.name
+   location            = azurerm_resource_group.terabitsrg.location
+   resource_group_name = azurerm_resource_group.terabitsrg.name
  }
  resource "azurerm_subnet" "terabitssubnet" {
    name                 = "terabits-sub"
-   resource_group_name  = azurerm_resource_group.myrg.name
-   virtual_network_name = azurerm_virtual_network.myvnet.name
+   resource_group_name  = azurerm_resource_group.terabitsrg.name
+   virtual_network_name = azurerm_virtual_network.terabitsrg.name
    address_prefixes     = ["10.0.2.0/24"]
  }
