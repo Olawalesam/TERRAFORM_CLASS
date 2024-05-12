3b6ccde309925ddb6fa725bff96bb4b26eaf7fae
Olawalesam <olawalesam@yahoo.com>
2024-05-12 23:36:18 +0100

    Second update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ resource "azurerm_virtual_network" "terabitsvnet" {
  resource "azurerm_subnet" "terabitssubnet" {
    name                 = "terabits-sub"
    resource_group_name  = azurerm_resource_group.terabitsrg.name
-   virtual_network_name = azurerm_virtual_network.terabitsrg.name
+   virtual_network_name = azurerm_virtual_network.terabitsvnet.name
    address_prefixes     = ["10.0.2.0/24"]
  }
